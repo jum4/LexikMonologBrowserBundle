251bf9057479e888d53d80e432f99de7f7fc2fd8
Julien Martin <julien.martin@jum4.org>
2018-04-01 08:03:46 +0200

    Update Configuration.php

--- DependencyInjection/Configuration.php
+++ DependencyInjection/Configuration.php
@@ -39,7 +39,6 @@ class Configuration implements ConfigurationInterface
                         ->scalarNode('table_name')->defaultValue('monolog_entries')->end()
                         ->scalarNode('connection_name')->end()
                         ->arrayNode('connection')
-                            ->cannotBeEmpty()
                             ->children()
                                 ->scalarNode('driver')->end()
                                 ->scalarNode('driverClass')->end()
